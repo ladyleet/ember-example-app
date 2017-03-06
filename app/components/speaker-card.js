898bcc8b86f31b3449765e6d0b2e73b4d5df3535
Tracy Lee <tracy@thisdot.co>
2017-03-06 02:52:36 -0800

    this branch is not good

---
+++ app/components/speaker-card.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    speakers: [{
+        name: "Ben Lesh",
+        twitter: "benlesh",
+        picture: "ben-lesh.jpg"
+    },{
+        name: "Alex Castillo",
+        twitter: "castillo__io",
+        picture: "alex-castillo.png"
+   },{
+        name: "Aysegul Yonet",
+        twitter: "ayssomething",
+        picture: "aysegul-yonet.png"
+   },{
+       name: "Aaron Frost",
+       twitter: "js_dev",
+       picture: "aaron-frost.jpg"
+  },{
+      name: "Gerard Sans",
+      twitter: "gerardsans",
+      picture: "gerard-sans.jpg"
+  },{
+      name: "Jeff Cross",
+      twitter: "jeffbcross",
+      picture: "jeff-cross.jpg"
+  },{
+      name: "Sebastian Witalec",
+      twitter: "sebawita",
+      picture: "sebastian-witalec.png"
+  },{
+      name: "Joe Eames",
+      twitter: "josepheames",
+      picture: "joe-eames.jpeg"
+  },{
+      name: "Tracy Lee",
+      twitter: "ladyleet",
+      picture: "tracy-lee.jpg"
+  },{
+      name: "Victor Savkin",
+      twitter: "victorsavkin",
+      picture: "victor-savkin.jpg"
+  }]
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('speakers');
+  this.route('sponsor');
+  this.route('about');
 });
 
 export default Router;
