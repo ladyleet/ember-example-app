86eb7cdf9e4467a891bdd34edd18c0dea0f42887
Tracy Lee <tracy@thisdot.co>
2017-03-06 03:34:10 -0800

    finally a good one

---
+++ app/components/speaker-card.js
@@ -0,0 +1,68 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+speakers: [{
+
+        name: "Ben Lesh",
+        twitter: "benlesh",
+        picture: "ben-lesh.jpg"
+    },{
+        name: "Alex Castillo",
+        twitter: "castillo__io",
+        picture: "alex-castillo.png"
+   },{
+        name: "Aysegul Yonet",
+        twitter: "ayssomething",
+        picture: "aysegul-yonet.png"
+   },{
+       name: "Aaron Frost",
+       twitter: "js_dev",
+       picture: "aaron-frost.jpg"
+  },{
+      name: "Gerard Sans",
+      twitter: "gerardsans",
+      picture: "gerard-sans.jpg"
+  },{
+      name: "Jeff Cross",
+      twitter: "jeffbcross",
+      picture: "jeff-cross.jpg"
+  },{
+      name: "Sebastian Witalec",
+      twitter: "sebawita",
+      picture: "sebastian-witalec.png"
+  },{
+      name: "Joe Eames",
+      twitter: "josepheames",
+      picture: "joe-eames.jpeg"
+  },{
+      name: "Tracy Lee",
+      twitter: "ladyleet",
+      picture: "tracy-lee.jpg"
+  },{
+      name: "Victor Savkin",
+      twitter: "victorsavkin",
+      picture: "victor-savkin.jpg"
+  },{
+        name: "Mike Brocchi",
+        twitter: "brocchi",
+        picture: "mike_brocchi.jpg"
+   },{
+        name: "Alyssa Nicoll",
+        twitter: "alyssanicoll",
+        picture: "alyssa_nicoll.jpg"
+   },{
+        name: "Minko Gechev",
+        twitter: "mgechev",
+        picture: "minko_gechev.jpg"
+   },{
+        name: "Justin Schwarty",
+        twitter: "schwarty",
+        picture: "justin_schwarty.jpg"
+   },{
+        name: "Uri Shaked",
+        twitter: "urishaked",
+        picture: "uri_shaked.jpg"
+   }]
+
+});
